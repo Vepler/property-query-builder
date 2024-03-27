b474061689d582e0021581940552686e69233dae
Goran Nikic <goran@nikic.co>
2024-03-27 06:38:06 +0000

    feat: initial version 1.0

---
+++ src/index.ts
@@ -0,0 +1,188 @@
+/**
+ * Supported comparators for query conditions.
+ */
+export type Comparator =
+  | 'eq'
+  | 'ne'
+  | 'gt'
+  | 'gte'
+  | 'lt'
+  | 'lte'
+  | 'in'
+  | 'nin'
+  | 'contains'
+  | 'startswith'
+  | 'endswith';
+
+/**
+ * Supported fields for query conditions.
+ */
+type SupportedFields =
+  | 'currentSaleListingPrice'
+  | 'currentRentalListingPrice'
+  | 'salePriceReductionPct'
+  | 'salePriceReductionAmt'
+  | 'propertyType'
+  | 'saleListingStatus'
+  | 'rentalListingStatus'
+  | 'daysOnMarketForSale'
+  | 'daysOnMarketForRent'
+  | 'beds'
+  | 'baths';
+
+/**
+ * Represents a single query condition.
+ */
+export type Condition = {
+  field: SupportedFields;
+  comparator: Comparator;
+  value: string | number | boolean | (string | number | boolean)[];
+};
+
+/**
+ * Represents a group of conditions.
+ */
+export type Group = {
+  conditions: Condition[];
+};
+
+/**
+ * Represents a query group with an operator and nested groups.
+ */
+export type QueryGroup = {
+  operator: 'AND' | 'OR';
+  groups: Group[];
+};
+
+/**
+ * Represents the overall query structure.
+ */
+export type Query = {
+  query: QueryGroup[];
+};
+
+/**
+ * QueryBuilder class for constructing and manipulating queries.
+ */
+export class QueryBuilder {
+  private query: Query = {
+    query: [],
+  };
+
+  /**
+   * Adds a new query group with the specified operator.
+   * @param operator The operator for the new query group ('AND' or 'OR').
+   * @returns The ID of the newly added group.
+   */
+  public addGroup(operator: 'AND' | 'OR'): string {
+    const groupId = this.generateGroupId();
+    const queryGroup: QueryGroup = {
+      operator,
+      groups: [],
+    };
+    this.query.query.push(queryGroup);
+    return groupId;
+  }
+
+  /**
+   * Removes a query group with the specified group ID.
+   * @param groupId The ID of the group to remove.
+   * @returns The QueryBuilder instance for chaining.
+   */
+  public removeGroup(groupId: string): QueryBuilder {
+    this.query.query = this.query.query.filter(
+      (queryGroup, index) => index !== parseInt(groupId, 10)
+    );
+    return this;
+  }
+
+  /**
+   * Adds a condition to the specified query group.
+   * @param groupId The ID of the group to add the condition to.
+   * @param condition The condition to add.
+   * @returns The QueryBuilder instance for chaining.
+   */
+  public addCondition(groupId: string, condition: Condition): QueryBuilder {
+    const queryGroup = this.query.query[parseInt(groupId, 10)];
+    if (queryGroup) {
+      queryGroup.groups.push({ conditions: [condition] });
+    }
+    return this;
+  }
+
+  /**
+   * Removes a condition from the specified query group.
+   * @param groupId The ID of the group to remove the condition from.
+   * @param conditionIndex The index of the condition to remove.
+   * @returns The QueryBuilder instance for chaining.
+   */
+  public removeCondition(
+    groupId: string,
+    conditionIndex: number
+  ): QueryBuilder {
+    const queryGroup = this.query.query[parseInt(groupId, 10)];
+    if (queryGroup) {
+      queryGroup.groups = queryGroup.groups.filter(
+        (group, index) => index !== conditionIndex
+      );
+    }
+    return this;
+  }
+
+  /**
+   * Updates a condition in the specified query group.
+   * @param groupId The ID of the group to update the condition in.
+   * @param conditionIndex The index of the condition to update.
+   * @param condition The updated condition.
+   * @returns The QueryBuilder instance for chaining.
+   */
+  public updateCondition(
+    groupId: string,
+    conditionIndex: number,
+    condition: Condition
+  ): QueryBuilder {
+    const queryGroup = this.query.query[parseInt(groupId, 10)];
+    if (queryGroup) {
+      queryGroup.groups[conditionIndex] = { conditions: [condition] };
+    }
+    return this;
+  }
+
+  /**
+   * Updates a query group with the specified operator and conditions.
+   * @param groupId The ID of the group to update.
+   * @param operator The updated operator for the query group.
+   * @param conditions The updated conditions for the query group.
+   * @returns The QueryBuilder instance for chaining.
+   */
+  public updateGroup(
+    groupId: string,
+    operator: 'AND' | 'OR',
+    conditions: Condition[]
+  ): QueryBuilder {
+    const queryGroup = this.query.query[parseInt(groupId, 10)];
+    if (queryGroup) {
+      queryGroup.operator = operator;
+      queryGroup.groups = conditions.map((condition) => ({
+        conditions: [condition],
+      }));
+    }
+    return this;
+  }
+
+  /**
+   * Retrieves the constructed query.
+   * @returns The constructed query.
+   */
+  public getQuery(): Query {
+    return this.query;
+  }
+
+  /**
+   * Generates a new group ID based on the current number of query groups.
+   * @returns The generated group ID.
+   */
+  private generateGroupId(): string {
+    return this.query.query.length.toString();
+  }
+}
---
+++ tests/queryBuilder.test.ts
@@ -0,0 +1,129 @@
+import { QueryBuilder, Condition, Query } from '../src';
+
+describe('QueryBuilder', () => {
+    let queryBuilder: QueryBuilder;
+
+    beforeEach(() => {
+        queryBuilder = new QueryBuilder();
+    });
+
+    test('should add a new query group with the specified operator', () => {
+        const groupId = queryBuilder.addGroup('AND');
+        expect(groupId).toBe('0');
+        expect(queryBuilder.getQuery()).toEqual({
+            query: [{ operator: 'AND', groups: [] }],
+        });
+    });
+
+    test('should remove a query group with the specified group ID', () => {
+        queryBuilder.addGroup('AND');
+        queryBuilder.addGroup('OR');
+        queryBuilder.removeGroup('1');
+        expect(queryBuilder.getQuery()).toEqual({
+            query: [{ operator: 'AND', groups: [] }],
+        });
+    });
+
+    test('should add a condition to the specified query group', () => {
+        const groupId = queryBuilder.addGroup('AND');
+        const condition: Condition = {
+            field: 'currentSaleListingPrice',
+            comparator: 'gt',
+            value: 100000,
+        };
+        queryBuilder.addCondition(groupId, condition);
+        expect(queryBuilder.getQuery()).toEqual({
+            query: [
+                {
+                    operator: 'AND',
+                    groups: [{ conditions: [condition] }],
+                },
+            ],
+        });
+    });
+
+    test('should remove a condition from the specified query group', () => {
+        const groupId = queryBuilder.addGroup('AND');
+        const condition: Condition = {
+            field: 'currentSaleListingPrice',
+            comparator: 'gt',
+            value: 100000,
+        };
+        queryBuilder.addCondition(groupId, condition);
+        queryBuilder.removeCondition(groupId, 0);
+        expect(queryBuilder.getQuery()).toEqual({
+            query: [{ operator: 'AND', groups: [] }],
+        });
+    });
+
+    test('should update a condition in the specified query group', () => {
+        const groupId = queryBuilder.addGroup('AND');
+        const condition: Condition = {
+            field: 'currentSaleListingPrice',
+            comparator: 'gt',
+            value: 100000,
+        };
+        queryBuilder.addCondition(groupId, condition);
+        const updatedCondition: Condition = {
+            field: 'currentSaleListingPrice',
+            comparator: 'lt',
+            value: 200000,
+        };
+        queryBuilder.updateCondition(groupId, 0, updatedCondition);
+        expect(queryBuilder.getQuery()).toEqual({
+            query: [
+                {
+                    operator: 'AND',
+                    groups: [{ conditions: [updatedCondition] }],
+                },
+            ],
+        });
+    });
+
+    test('should update a query group with the specified operator and conditions', () => {
+        const groupId = queryBuilder.addGroup('AND');
+        const conditions: Condition[] = [
+            {
+                field: 'currentSaleListingPrice',
+                comparator: 'gt',
+                value: 100000,
+            },
+            {
+                field: 'beds',
+                comparator: 'gte',
+                value: 3,
+            },
+        ];
+        queryBuilder.updateGroup(groupId, 'OR', conditions);
+        expect(queryBuilder.getQuery()).toEqual({
+            query: [
+                {
+                    operator: 'OR',
+                    groups: [
+                        { conditions: [conditions[0]] },
+                        { conditions: [conditions[1]] },
+                    ],
+                },
+            ],
+        });
+    });
+
+    test('should retrieve the constructed query', () => {
+        const groupId = queryBuilder.addGroup('AND');
+        const condition: Condition = {
+            field: 'currentSaleListingPrice',
+            comparator: 'gt',
+            value: 100000,
+        };
+        queryBuilder.addCondition(groupId, condition);
+        const query: Query = {
+            query: [
+                {
+                    operator: 'AND',
+                    groups: [{ conditions: [condition] }],
+                },
+            ],
+        };
+        expect(queryBuilder.getQuery()).toEqual(query);
+    });
+});
